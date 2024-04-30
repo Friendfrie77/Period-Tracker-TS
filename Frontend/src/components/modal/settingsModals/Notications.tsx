18ad56cd9c7bcc2e25945496765b4567fee57bd0
Albert Friend <awfriend77@gmail.com>
2024-04-30 17:20:32 -0400

    working on settings page

---
+++ Frontend/src/components/modal/settingsModals/Notications.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import type { functionType } from "../modals.types";
+import { AiOutlineClose } from "react-icons/ai";
+import useUserInfo from '../../../hooks/useUserInfo';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/high-res.css';
+const Notication:React.FC<functionType> =({modalFunction}) =>{
+    const {notifications} = useUserInfo();
+    const [phoneNumber, setPhoneNumber] = useState('')
+    const numberChange = (number:string) =>{
+        setPhoneNumber(number)
+    }
+    const content = (
+        <div className="modal-box">
+            <AiOutlineClose className="exit-button right-align" onClick={modalFunction} />
+            <div className="flex-center box-padding">
+                {notifications ?(
+                    <div className="flex-center">
+                    <h1>Would you like to stop notifications?</h1>
+                    <button className="button" type="submit">Submit</button>
+                    </div>
+                ): (
+                    <div className="flex-center box-padding">
+                    <h1>Would you like to sign up for text notifications?</h1>
+                    <p>With notifications you will get a text a few days before your period starts and a few days before it ends.</p>
+                    <PhoneInput
+                        // containerClass="flex-center"
+                        containerStyle={{width:'auto'}}
+                        onChange={numberChange}
+                        country={'us'}
+                        value={phoneNumber}
+                    />
+                    <button className="button" type="submit">Submit</button>
+                    </div>
+                )
+                }
+                
+            </div>
+        </div>
+    )
+
+    return content
+}
+export default Notication
--- Frontend/src/components/modal/settingsModals/Settings.tsx
+++ Frontend/src/components/modal/settingsModals/Settings.tsx
@@ -3,29 +3,38 @@ import useUserInfo from "../../../hooks/useUserInfo";
 import { AiOutlineClose } from "react-icons/ai";
 import DeleteAccount from "./DeleteAccount";
 import ChangePassword from "./ChangePassword";
+import Notication from "./Notications";
 type SettingsProps ={
     setSettingOpen: () => void 
 }
 const SettingsModal: React.FC<SettingsProps> = ({setSettingOpen}) =>{
+    const [settingsModelOpen, setModelOpen] = useState(false)
     const [deleteModal, setDeleteOpen] = useState(false);
     const openDelete = () =>{
         setDeleteOpen(!deleteModal);
+        setModelOpen(!settingsModelOpen)
     }
     const [passwordModal, setPasswordOpen] = useState(false);
     const openPassword = () =>{
         setPasswordOpen(!passwordModal)
+        setModelOpen(!settingsModelOpen)
+    }
+    const [notificationModal, setNotificationOpen] = useState(false);
+    const openNotification = () =>{
+        setNotificationOpen(!notificationModal)
+        setModelOpen(!settingsModelOpen)
     }
     const {role} = useUserInfo();
     const content = (
         <section>
-            <div className={`modal-box ${deleteModal || passwordModal ? 'display-none' : ''}`}>
+            <div className={`modal-box ${settingsModelOpen ? 'display-none' : ''}`}>
                 <AiOutlineClose className="exit-button right-align" onClick={setSettingOpen}/>
             {role === "Guest" && <h1 className="center-align">Some settings will not be avilable on a guest account</h1>}
                 <div className="setting-option center-align">
                     <button className="button" disabled = {role != "Guest"} onClick={openPassword}>Change Password</button>
                 </div>
                 <div className="setting-option">
-                    <button className="button" disabled = {role === "Guest"}>Toggle Notication</button>
+                    <button className="button" disabled = {role != "Guest"} onClick={openNotification}>Toggle Notication</button>
                 </div>
                 <div className="setting-option">
                     <button className="button" onClick={openDelete}>Delete Account</button>
@@ -36,6 +45,9 @@ const SettingsModal: React.FC<SettingsProps> = ({setSettingOpen}) =>{
         }
         {passwordModal &&
             <ChangePassword modalFunction={openPassword}/>
+        }
+        {notificationModal &&
+            <Notication modalFunction={openNotification} />
         }
         </section>
     )
