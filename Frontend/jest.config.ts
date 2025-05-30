2b7d0151ebdef293275a4149228af9265664b3ea
Albert Friend <awfriend77@gmail.com>
2025-05-30 16:08:18 -0400

    reworking test to use vitest

--- Frontend/jest.config.ts
+++ Frontend/jest.config.ts
@@ -14,6 +14,7 @@ const config: Config.InitialOptions = {
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    // '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/src/test/fileMock.ts'
   },
   setupFilesAfterEnv:['<rootDir>/src/tests/setupTest.ts']
 };
